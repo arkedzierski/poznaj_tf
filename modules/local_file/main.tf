28020ddf37dfbd9d2e55939f54385b37ee8bbc98
Artur Kedzierski <akedzierski@pgs-soft.com>
2023-08-03 22:05:01 +0200

    Modules local_file resource name changed - 0.0.2 - fix3

--- modules/local_file/main.tf
+++ modules/local_file/main.tf
@@ -11,7 +11,7 @@ resource "local_file" "file" {
 }
 
 moved {
-  from = local_file.local_sensitive_file
+  from = local_sensitive_file.local_sensitive_file
   to   = local_sensitive_file.sensitive_filee
 }
 

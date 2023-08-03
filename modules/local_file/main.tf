149e2d76b5e3245822dc2456a51aeb93d076f89b
arkedzierski <45371700+arkedzierski@users.noreply.github.com>
2023-08-03 22:23:07 +0200

    version 0.0.2 (#2)
    * Modules 1 - 4
    
    * Modules 1 - 4
    
    * Modules local_file resource name changed - 0.0.2
    
    * Modules local_file resource name changed - 0.0.2 - fix
    
    * Modules local_file resource name changed - 0.0.2 - fix2
    
    * Modules local_file resource name changed - 0.0.2 - fix3
    
    * Modules local_file resource name changed - 0.0.2 - fix4
    
    * Module4 complited and files clean up
    
    ---------
    
    Co-authored-by: Artur Kedzierski <akedzierski@pgs-soft.com>

--- modules/local_file/main.tf
+++ modules/local_file/main.tf
@@ -1,11 +1,21 @@
-resource "local_file" "local_file" {
+moved {
+  from = local_file.local_file
+  to   = local_file.file
+}
+
+resource "local_file" "file" {
   count = var.is_sensitive ? 0 : 1
 
   content  = var.content_base64 == "" ? var.content : base64encode(var.content_base64)
   filename = var.filename
 }
 
-resource "local_sensitive_file" "local_sensitive_file" {
+moved {
+  from = local_sensitive_file.local_sensitive_file
+  to   = local_sensitive_file.sensitive_file
+}
+
+resource "local_sensitive_file" "sensitive_file" {
   count = var.is_sensitive ? 1 : 0
 
   content  = var.content_base64 == "" ? var.content : base64encode(var.content_base64)
--- modules/local_file/outputs.tf
+++ modules/local_file/outputs.tf
@@ -1,3 +1,3 @@
 output "id" {
-  value = var.is_sensitive ? local_sensitive_file.local_sensitive_file[0].id : local_file.local_file[0].id
+  value = var.is_sensitive ? local_sensitive_file.sensitive_file[0].id : local_file.file[0].id
 }

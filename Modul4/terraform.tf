e2a385093a529dac774b750bdb8eff828932846d
Artur Kedzierski <akedzierski@pgs-soft.com>
2023-08-03 22:11:54 +0200

    Module4 complited and files clean up

--- Modul4/terraform.tf
+++ Modul4/terraform.tf
@@ -1,12 +1,12 @@
 module "file" {
-  source           = "git::https://github.com/arkedzierski/poznaj_tf.git//modules/local_file?ref=dev"
+  source           = "git::https://github.com/arkedzierski/poznaj_tf.git//modules/local_file?ref=0.0.2"
   filename         = "test.txt"
   content          = "testowy plik"
   file_permissions = "755"
 }
 
 module "file_sensitive" {
-  source         = "git::https://github.com/arkedzierski/poznaj_tf.git//modules/local_file?ref=dev"
+  source         = "git::https://github.com/arkedzierski/poznaj_tf.git//modules/local_file?ref=0.0.2"
   filename       = "test_sensitive.txt"
   content_base64 = "testowy plik sensitive"
   is_sensitive   = true
